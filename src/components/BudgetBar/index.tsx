ad78cd85a7b58058a14c21cf3b0142b3461b466d
FernandoGaspar <fernando.gaspar94@gmail.com>
2022-12-29 20:48:46 -0300

    Inclusão da visão do budget

---
+++ src/components/BudgetBar/index.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useMemo, useState } from 'react';
+
+
+import BudgetBarLine from '../BudgetBarLine';
+
+import axios from 'axios';
+import { URL_API } from '../../repositories/baseAPI';
+
+import { 
+    Container,
+ }  from './styles';
+
+ 
+interface IDataBudgetVrsRealizado {
+    AnoMes: string
+    subGrupoContaContabil: string
+    Cor: string
+    ValorOrcado: number
+    ValorRealizado: number
+    Delta: number    
+  }
+
+
+const BudgetBar: React.FC = () => { 
+    
+
+    const [dataBudgetVrsRealizado, setDataBudgetVrsRealizado] = useState<IDataBudgetVrsRealizado[]>([]);
+    const idUsuario = localStorage.getItem('@minha-carteira:usuarioId') as string;
+  
+    const getBudgetVrsRealizado = () => {
+        axios.post (URL_API + "/budgetvrsRealizado", {
+            headers: {"Access-Control-Allow-Origin": "*"},
+            anomes: "202212",
+            usuario: idUsuario
+        })
+        .then((response) => {
+            const { data } = response
+            setDataBudgetVrsRealizado (JSON.parse(data))
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+
+    useEffect(() => {
+
+        getBudgetVrsRealizado()
+        console.log (dataBudgetVrsRealizado)
+    },[]);
+
+    return (
+        <Container>
+            {
+                dataBudgetVrsRealizado.map(item => (
+                    <BudgetBarLine 
+                        grupoContaContabil={item.subGrupoContaContabil} 
+                        valorRestante={Number(item.ValorOrcado-item.ValorRealizado)} 
+                        valorBudget={Number(item.ValorOrcado)} 
+                        valorRealizado={Number(item.ValorRealizado)} 
+                        progress = {item.Delta*100}
+                        cor={item.Cor}/>
+                ))
+            } 
+            
+        </Container>
+    );  
+}
+export default BudgetBar;
+
