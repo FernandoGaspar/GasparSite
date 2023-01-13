b6c130db4d0c211fddc5bda10b07e5f3ced0e006
FernandoGaspar <fernando.gaspar94@gmail.com>
2023-01-13 18:46:17 -0300

    Ajuste do grafico de pizza

--- src/components/PieChartBox/styles.ts
+++ src/components/PieChartBox/styles.ts
@@ -1,5 +1,9 @@
 import styled, { keyframes } from 'styled-components';
 
+interface ITitleContainerProps {
+    lineColor: string;
+}
+
 const animate = keyframes`
     0% {
         transform: translateX(100px);
@@ -18,24 +22,77 @@ const animate = keyframes`
 export const Container = styled.div`
     width: 100%;
     height: 260px;
-
-    margin: 10px 0;
-
-    > h2 {
-        margin-bottom: 20px;
-        margin: 4%;
-    }
-    > span {
-        margin: 4px 4px 4px 4px;
-        font-size: 18px;
-        font-weight: 500;
-    }
+    margin: 10px 0px 0px 0;
 
     background-color: ${props => props.theme.colors.tertiary};
     color: ${props => props.theme.colors.white};
-
     border-radius: 7px;
-
     animation: ${animate} .5s;
+    display: flex;
+    overflow-y: visible;
+
+    > section {
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        margin: 10px 15px;
+    }
+
+    > section span {
+        overflow: visible!important;
+        margin: 0px -90px 0px 0px;
+    }
+
+    > section main {
+        margin: 65px 0px;
+
+        height: 150px;
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+
+        padding-bottom: 0;
+        overflow-x: scroll;
+
+        ::-webkit-scrollbar {
+            width: 10px;
+        }
+    
+        ::-webkit-scrollbar-thumb {
+            background-color: ${props => props.theme.colors.secondary};
+            border-radius: 10px;
+        }
+    
+        ::-webkit-scrollbar-track {
+            background-color: ${props => props.theme.colors.tertiary};
+        }
+    }
+`;
+
+export const TituloGrupo = styled.div<ITitleContainerProps>`
+    margin: 3px;
+    > footer {
+        border-top: 5px solid ${props => props.lineColor};
+        width: 55px;
+        font-size: 12px;
+        margin: 0px;
+        content: '';
+    }
+    > h1 {
+
+        font-size: 10px;
+        margin: 0px;
+    }
+    @media(max-width: 420px){
+        > footer {
+                &::after {
+                content: '';
+                display: block;
+                width: 35px;
+                margin: 0px;
+                border-top: 3px solid ${props => props.lineColor};
+            }
+        }
+    }
 
 `;
