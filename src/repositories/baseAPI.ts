898fa57b82e615958d3ba2a02aa09bb126d83656
Fernando Gaspar <fernando.gaspar94@gmail.com>
2023-03-13 15:37:39 -0300

    Ataulização

--- src/repositories/baseAPI.ts
+++ src/repositories/baseAPI.ts
@@ -1,14 +1,14 @@
 export const URL_AMBIENTE = window.location.href;
 
 export const URL_API_PRD_EXT = "http://fernandogasparjr.ddns.net:8075";
-export const URL_API_PRD_INT = "http://192.168.15.201:8075";
+export const URL_API_PRD_INT = "http://192.168.2.187:8075";
 export const URL_API = URL_AMBIENTE!.includes("fernandogaspar") ? URL_API_PRD_EXT : URL_API_PRD_INT;
 
 // export const URL_API = URL_API_PRD_EXT;
 
 
 export const URL_API_HOME_PRD_EXT = "http://fernandogasparjr.ddns.net:9077";
-export const URL_API_HOME_PRD_INT = "http://192.168.15.201:9077";
+export const URL_API_HOME_PRD_INT = "http://192.168.2.187:9077";
 export const URL_API_HOME = URL_AMBIENTE!.includes("fernandogaspar") ? URL_API_HOME_PRD_EXT : URL_API_HOME_PRD_INT;
 
 // export const URL_API_HOME = URL_API_HOME_PRD_EXT;
